c2b349365aa662e1590fe3e2816db7ca859d6001
RichardMartinAUS <richard.martin001@gmail.com>
2022-07-30 15:50:26 +1000

    added inquirer questions

--- index.js
+++ index.js
@@ -2,3 +2,89 @@
 // separate answers to various classes
 // collect classes to build back into the html template
 // render and write the html file
+
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Empty array for the user responses to build with
+const userAnswers = [];
+
+// Questions for user using inquierer
+const questions = [
+    {
+        type: 'list',
+        name: 'employeeType',
+        message: 'What type of employee would you like to enter?',
+        choices: [
+            'Manager',
+            'Engineer',
+            'Intern',
+        ],
+    },
+    {
+        type: 'input',
+        name: 'employeeName',
+        message: 'Enter the first name of the employee.',
+    },
+    {
+        type: 'input',
+        name: 'employeeId',
+        message: 'Enter the ID of the employee.',
+    },
+    {
+        type: 'input',
+        name: 'employeeEmail',
+        message: 'Enter the email address of the employee.',
+    },
+    {
+        type: 'input',
+        name: 'employeeOffice',
+        message: 'Enter the office number of the employee.',
+        when: (answers) => answers.employeeType === 'Manager',
+    },
+    {
+        type: 'input',
+        name: 'employeeGithub',
+        message: 'Enter the GitHub username of the employee.',
+        when: (answers) => answers.employeeType === 'Engineer',
+    },
+    {
+        type: 'input',
+        name: 'employeeSchool',
+        message: 'What school is the employee attending?',
+        when: (answers) => answers.employeeType === 'Intern',
+    },
+    {
+        type: 'confirm',
+        name: 'askAgain',
+        message: 'Would you like to add another team member? (hit enter for YES)',
+        default: true,
+    },
+];
+
+
+// Function to ask the questions and filter according to answers
+function askQuestions() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        userAnswers.push(answers);
+        if (answers.askAgain) {
+            askQuestions();
+        } else {
+            console.log('Your team is made up of:' + JSON.stringify(userAnswers));
+            module.exports(userAnswers);
+        }
+    })
+}
+
+
+
+// Functions to call when the app is initialized by index.js
+function init() {
+    console.log('Welcome to the team-builder app! \nPlease answer the following questions and an HTML \ndocument will be created based on your answers.');
+    askQuestions();
+}
+
+// Function call to initialize app
+init();
